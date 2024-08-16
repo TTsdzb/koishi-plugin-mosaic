e9afbb21ccd1149645012d3e3acb123bcd4ba486
TTsdzb <ttsdzb@outlook.com>
2024-08-16 21:01:59 +0800

    feat: initial commit

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-mosaic?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-mosaic)
 
-适用于Koishi的头像马赛克插件
+适用于 Koishi 的头像马赛克插件。貌似没什么用，仅用于娱乐。
---
+++ src/index.tsx
@@ -0,0 +1,41 @@
+import { Context, Schema } from "koishi";
+import {} from "koishi-plugin-canvas";
+
+export const name = "mosaic";
+export const inject = ["canvas"];
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({});
+
+async function scaleImage(ctx: Context, imageUrl: string): Promise<string> {
+  const CANVAS_SIZE = 16;
+
+  const image = await ctx.canvas.loadImage(imageUrl);
+  const canvas = await ctx.canvas.createCanvas(CANVAS_SIZE, CANVAS_SIZE);
+  const canvasCtx = canvas.getContext("2d");
+
+  canvasCtx.drawImage(image, 0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+  return await canvas.toDataURL("image/png");
+}
+
+export function apply(ctx: Context) {
+  ctx.i18n.define("zh-CN", require("./locales/zh-CN"));
+
+  ctx.command("mosaic <target:user>").action(async ({ session }, target) => {
+    const targetUser = target
+      ? await session.bot.getUser(target.split(":")[1])
+      : session.event.user;
+    const image = await scaleImage(ctx, targetUser.avatar);
+
+    return (
+      <html>
+        <img
+          style="image-rendering: pixelated; width: 512px; height: 512px"
+          src={image}
+        />
+      </html>
+    );
+  });
+}
